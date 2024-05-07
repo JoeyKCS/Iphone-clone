404aff9f036e6ae50152fe1c9d6fd62f6f3dd4ae
Mohau <106928210+JoeyKCS@users.noreply.github.com>
2024-05-07 11:57:09 +0200

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import Highlights from './components/Highlights';
+import Model from './components/Model';
+
+
+
+import './index.css'
+
+const App = () => {
+
+
+  return (
+   <main className="bg-black">
+    <Navbar />
+    <Hero />
+    <Highlights />
+    <Model />
+
+
+   </main>
+)
+}
+
+export default App
